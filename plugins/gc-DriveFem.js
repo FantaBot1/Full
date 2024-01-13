8b877f4d332563794da9e133c2ef90dc5b6ff58d
FantaBot1 <155099912+FantaBot1@users.noreply.github.com>
2024-01-13 01:12:26 +0100

    Add files via upload

---
+++ plugins/gc-DriveFem.js
@@ -0,0 +1,11 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.DriveFem);
+};
+handler.command = /^(DriveFem|DriveFem)$/i;
+export default handler;
+
+global.DriveFem = `
+〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕
+
+*LINK :* https://drive.google.com/drive/mobile/folders/17qXegvUYmXU0nYnMUJKJTSzGcChjQXyH
+`;
